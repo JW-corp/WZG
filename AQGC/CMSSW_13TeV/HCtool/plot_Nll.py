74979628cb816bfe1f7088a2c38e506d225320e5
ico1036 <jiwoong.kim@cern.ch>
2022-04-14 15:25:27 +0900

    NLL scan

--- AQGC/CMSSW_13TeV/HCtool/plot_Nll.py
+++ AQGC/CMSSW_13TeV/HCtool/plot_Nll.py
@@ -18,23 +18,46 @@ deltaNLL_95 = 3.8414588206941236
 # I&O , Convert tree to arrays (First value is ignored)
 infile="/x5/cms/jwkim/HCtool/CMSSW_10_2_13/src/HiggsAnalysis/CombinedLimit/data/tutorials/higgsCombineTest.MultiDimFit.mH120.root"
 tree	= uproot.open(infile+":/limit")
-arr_FT0		 = tree['FT0'].array()[:-1]
-arr_deltaNLL = tree['deltaNLL'].array()[:-1] * 2
+arr_FT0		 = tree['FT0'].array()[1:]
+arr_deltaNLL = tree['deltaNLL'].array()[1:] * 2
+
 
 
 # Find CLs 95% x 
-x_95=0
-y_95=0
+mx_95=0
+my_95=0
+px_95=0
+py_95=0
+
+
+convex_cnt=0
 for i in range(len(arr_deltaNLL)):
-	if arr_deltaNLL[i] >= deltaNLL_95:
-		x_95 = arr_FT0[i]
-		y_95 = arr_deltaNLL[i]
+
+	# negative
+	print(arr_deltaNLL[i])
+	if (arr_deltaNLL[i] <= deltaNLL_95) and (convex_cnt==0):
+		mx_95 = arr_FT0[i]
+		my_95 = arr_deltaNLL[i]
+		convex_cnt+=1
+		print(mx_95,my_95)
+
+	# positive
+	if (arr_deltaNLL[i] >= deltaNLL_95) and (convex_cnt==1):
+		px_95 = arr_FT0[i]
+		py_95 = arr_deltaNLL[i]
+		print(px_95,py_95)
 		break
+
+
+
 
 # Plotting
 plt.plot(arr_FT0,arr_deltaNLL,'.',color='royalblue',label='expected 2$\Delta$NLL')
+
 plt.axhline(y=deltaNLL_95,color='black', linewidth=3,linestyle='--') # horizon line 95% CLs
-plt.vlines(x_95,min(arr_deltaNLL)-1,y_95,linewidth=3,linestyle='-',color='black',label='expected 95% confidence limit \n[-{0:.3f},{0:.3f}]'.format(x_95))
+
+plt.vlines(mx_95,-1,my_95,linewidth=3,linestyle='-',color='black',label='expected 95% confidence limit \n[{0:.3f},{1:.3f}]'.format(mx_95,px_95))
+plt.vlines(px_95,-1,py_95,linewidth=3,linestyle='-',color='black')
 
 
 plt.ylim(min(arr_deltaNLL)-1,max(arr_deltaNLL)+1)
