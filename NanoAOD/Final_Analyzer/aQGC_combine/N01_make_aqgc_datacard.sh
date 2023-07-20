d2ee549007ade87b1dda9ce7dabb39ed49564791
ico1036 <jiwoong.kim@cern.ch>
2023-07-20 15:13:30 +0900

    aQGC new-version combined tool

---
+++ NanoAOD/Final_Analyzer/aQGC_combine/N01_make_aqgc_datacard.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# previous
+#f=/cms/ldap_home/jwkim2/New_ccp/plot/2018/aQGC_dim8/Summary_for_HCtool/WZG_2018_Rwt0.root
+
+# After re-new
+f=/cms/ldap_home/jwkim2/New_ccp/HCtool_new/CMSSW_10_2_13/src/HiggsAnalysis/CombinedLimit/data/tutorials/WZG/dim8/2018/aQGC_2018.root
+
+
+python Combine_help.py $f
+
+
